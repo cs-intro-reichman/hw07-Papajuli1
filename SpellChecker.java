e9d11bcb55bd508bd7ab9e0ab9675c816d087763
Papajuli1 <yonatan.abramovich@post.runi.ac.il>
2024-01-30 09:25:21 +0200

    El Final

--- SpellChecker.java
+++ SpellChecker.java
@@ -18,9 +18,9 @@ public class SpellChecker {
 
 	public static int levenshtein(String word1, String word2) {
 		if (word1.length() == 0)
-			return word1.length();
-		if (word2.length() == 0)
 			return word2.length();
+		if (word2.length() == 0)
+			return word1.length();
 		if (word1.charAt(0) == word2.charAt(0))
 			return levenshtein(tail(word1), tail(word2));
 		else {
